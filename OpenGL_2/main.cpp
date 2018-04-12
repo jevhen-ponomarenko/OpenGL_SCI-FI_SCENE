8da7e07cab8f42774f0bcf2b974824867bd6eefc
UG <ponomrenkodesk@gmail.com>
2018-04-12 08:39:30 +0200

    model loading

--- OpenGL_2/main.cpp
+++ OpenGL_2/main.cpp
@@ -11,6 +11,7 @@
 // GL includes
 #include "Shader.h"
 #include "Camera.h"
+#include "Model.h"
 
 // GLM Mathemtics
 #include <glm/glm.hpp>
@@ -22,44 +23,35 @@
 
 // Properties
 const GLuint WIDTH = 800, HEIGHT = 600;
-int SCREEN_WIDTH , SCREEN_HEIGHT;
+int SCREEN_WIDTH, SCREEN_HEIGHT;
 
 // Function prototypes
 void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode );
-// void ScrollCallback( GLFWwindow *window, double xOffset, double yOffset );
 void MouseCallback( GLFWwindow *window, double xPos, double yPos );
 void DoMovement( );
-void CameraCallback(int cameraMode);
 
 // Camera
-Camera  camera(glm::vec3( 0.0f, 0.0f, 3.0f ) );
-
-bool freeCameraMode = true;
-
-GLfloat lastX = WIDTH / 2.0;
-GLfloat lastY = HEIGHT / 2.0;
+Camera camera( glm::vec3( 0.0f, 0.0f, 3.0f ) );
 bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
 bool firstMouse = true;
-int cameraMode = 0;
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
-
-glm::vec3 lightPos (0.2f, 1.0f, 10.0f);
-
-// The MAIN function, from here we start our application and run our Game loop
 int main( )
 {
     // Init GLFW
     glfwInit( );
+    // Set all the required options for GLFW
     glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
     glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
-    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
+    glfwWindowHint( GLFW_RESIZABLE, GL_FALSE );
     
-    GLFWwindow* window = glfwCreateWindow( WIDTH, HEIGHT, "UG's scene", nullptr, nullptr ); // Windowed
+    // Create a GLFWwindow object that we can use for GLFW's functions
+    GLFWwindow *window = glfwCreateWindow( WIDTH, HEIGHT, "UG's scene", nullptr, nullptr );
     
     if ( nullptr == window )
     {
@@ -75,10 +67,9 @@ int main( )
     
     // Set the required callback functions
     glfwSetKeyCallback( window, KeyCallback );
-        glfwSetCursorPosCallback( window, MouseCallback );
-   // glfwSetScrollCallback( window, ScrollCallback );
+    glfwSetCursorPosCallback( window, MouseCallback );
     
-    // Options, removes the mouse cursor for a more immersive experience
+    // GLFW Options
     glfwSetInputMode( window, GLFW_CURSOR, GLFW_CURSOR_DISABLED );
     
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
@@ -93,202 +84,87 @@ int main( )
     // Define the viewport dimensions
     glViewport( 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT );
     
-    // Setup some OpenGL options
+    // OpenGL options
     glEnable( GL_DEPTH_TEST );
     
-    // enable alpha support
-    glEnable( GL_BLEND );
-    glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
-    
     // Setup and compile our shaders
-    Shader lampShader( "resources/shaders/lamp.vs", "resources/shaders/lamp.frag" );
-    Shader lightingShader( "resources/shaders/lighting.vs", "resources/shaders/lighting.frag" );
-
+    Shader shader( "resources/shaders/modelLoading.vs", "resources/shaders/modelLoading.frag" );
+    Shader shader2( "resources/shaders/modelLoading.vs", "resources/shaders/modelLoading.frag" );
     
-    // Set up our vertex data (and buffer(s)) and attribute pointers
-
-    GLfloat vertices[] =
-    {
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        
-        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-        
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-    };
-
+    // Load models
+    Model ourModel( "resources/models/nanosuit.obj" );
+    Model ourModel2( "resources/models/hammer/martillothor.obj" );
     
-  
-    GLuint VBO, containerVAO;
-    glGenVertexArrays( 1, &containerVAO );
-    glGenBuffers( 1, &VBO );
+    // Draw in wireframe
+    //glPolygonMode( GL_FRONT_AND_BACK, GL_LINE );
     
-    glBindBuffer( GL_ARRAY_BUFFER, VBO );
-    glBufferData( GL_ARRAY_BUFFER, sizeof( vertices ), vertices, GL_STATIC_DRAW );
+    glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( float )SCREEN_WIDTH/( float )SCREEN_HEIGHT, 0.1f, 100.0f );
     
-    glBindVertexArray( containerVAO );
-    // Position attribute
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( GLvoid * )0 );
-    glEnableVertexAttribArray( 0 );
-    
-    // Normal attribute
-    glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( GLvoid * )( 3 * sizeof( GLfloat ) ) );
-    glEnableVertexAttribArray( 1 );
-    glBindVertexArray( 0 );
-    
-    // Then, we set the light's VAO (VBO stays the same. After all, the vertices are the same for the light object (also a 3D cube))
-    GLuint lightVAO;
-    glGenVertexArrays( 1, &lightVAO );
-    glBindVertexArray( lightVAO );
-    // We only need to bind to the VBO (to link it with glVertexAttribPointer), no need to fill it; the VBO's data already contains all we need.
-    glBindBuffer( GL_ARRAY_BUFFER, VBO );
-    // Set the vertex attributes (only position data for the lamp))
-    glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), ( GLvoid * )0 ); // Note that we skip over the normal vectors
-    glEnableVertexAttribArray( 0 );
-    glBindVertexArray( 0 );
-    
-    glm::mat4 projection = glm::perspective( camera.GetZoom( ), ( GLfloat )SCREEN_WIDTH / ( GLfloat )SCREEN_HEIGHT, 0.1f, 100.0f );
     // Game loop
     while( !glfwWindowShouldClose( window ) )
     {
-        // add after the code is displayed and working
-        //lightPos.x -= 0.01f;
-        //lightPos.z -= 0.01f;
-        
-        // Calculate deltatime of current frame
+        // Set frame time
         GLfloat currentFrame = glfwGetTime( );
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
         
-        //camera changing
-        //CameraCallback(cameraMode);
-        
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        // Check and call events
         glfwPollEvents( );
-    
         DoMovement( );
         
         // Clear the colorbuffer
-        glClearColor( 0.1f, 0.1f, 0.1f, 1.0f );
+        glClearColor( 0.05f, 0.05f, 0.05f, 1.0f );
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
         
-        // Use cooresponding shader when setting uniforms/drawing objects
-        lightingShader.Use( );
-        GLint objectColorLoc = glGetUniformLocation( lightingShader.Program, "objectColor" );
-        GLint lightColorLoc = glGetUniformLocation( lightingShader.Program, "lightColor" );
-        GLint lightPosLoc = glGetUniformLocation( lightingShader.Program, "lightPos" );
-        GLint viewPosLoc = glGetUniformLocation( lightingShader.Program, "viewPos" );
-        glUniform3f( objectColorLoc, 1.0f, 0.5f, 0.31f );
-        glUniform3f( lightColorLoc, 1.0f, 1.0f, 1.0f );
-        glUniform3f( lightPosLoc, lightPos.x, lightPos.y, lightPos.z );
-        glUniform3f( viewPosLoc, camera.GetPosition( ).x, camera.GetPosition( ).y, camera.GetPosition( ).z );
+        shader.Use( );
+        shader2.Use();
         
-        // Create camera transformations
-        glm::mat4 view;
-        view = camera.GetViewMatrix( );
+        glm::mat4 view = camera.GetViewMatrix( );
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "projection" ), 1, GL_FALSE, glm::value_ptr( projection ) );
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "view" ), 1, GL_FALSE, glm::value_ptr( view ) );
         
-        // Get the uniform locations
-        GLint modelLoc = glGetUniformLocation( lightingShader.Program, "model" );
-        GLint viewLoc = glGetUniformLocation( lightingShader.Program, "view" );
-        GLint projLoc = glGetUniformLocation( lightingShader.Program, "projection" );
-        
-        // Pass the matrices to the shader
-        glUniformMatrix4fv( viewLoc, 1, GL_FALSE, glm::value_ptr( view ) );
-        glUniformMatrix4fv( projLoc, 1, GL_FALSE, glm::value_ptr( projection ) );
-        
-        // Draw the container (using container's vertex attributes)
-        glBindVertexArray( containerVAO );
+        // Draw the loaded model
         glm::mat4 model;
-        glUniformMatrix4fv( modelLoc, 1, GL_FALSE, glm::value_ptr( model ) );
-        glDrawArrays( GL_TRIANGLES, 0, 36 );
-        glBindVertexArray( 0 );
+        glm::mat4 model2;
+        model = glm::translate( model, glm::vec3( 0.0f, -1.75f, 0.0f ) ); // Translate it down a bit so it's at the center of the scene
+        model = glm::scale( model, glm::vec3( 0.2f, 0.2f, 0.2f ) );    // It's a bit too big for our scene, so scale it down
+        glUniformMatrix4fv( glGetUniformLocation( shader.Program, "model" ), 1, GL_FALSE, glm::value_ptr( model ) );
         
-        // Also draw the lamp object, again binding the appropriate shader
-        lampShader.Use( );
-        // Get location objects for the matrices on the lamp shader (these could be different on a different shader)
-        modelLoc = glGetUniformLocation( lampShader.Program, "model" );
-        viewLoc = glGetUniformLocation( lampShader.Program, "view" );
-        projLoc = glGetUniformLocation( lampShader.Program, "projection" );
-        // Set matrices
-        glUniformMatrix4fv( viewLoc, 1, GL_FALSE, glm::value_ptr( view ) );
-        glUniformMatrix4fv( projLoc, 1, GL_FALSE, glm::value_ptr( projection ) );
-        model = glm::mat4( );
-        model = glm::translate( model, lightPos );
-        model = glm::scale( model, glm::vec3( 0.2f ) ); // Make it a smaller cube
-        glUniformMatrix4fv( modelLoc, 1, GL_FALSE, glm::value_ptr( model ) );
-        // Draw the light object (using light's vertex attributes)
-        glBindVertexArray( lightVAO );
-        glDrawArrays( GL_TRIANGLES, 0, 36 );
-        glBindVertexArray( 0 );
+        model2 = glm::translate( model2, glm::vec3( 5.0f, -1.75f, 0.0f ) ); // Translate it down a bit so it's at the center of the scene
+       // model2 = glm::scale( model2, glm::vec3( 20.0f, 20.0f, 20.0f ) );    // It's a bit too big for our scene, so scale it down
+        glUniformMatrix4fv( glGetUniformLocation( shader2.Program, "model" ), 1, GL_FALSE, glm::value_ptr( model2 ) );
         
-        // Swap the screen buffers
+        ourModel.Draw( shader );
+        ourModel2.Draw( shader2 );
+        
+        // Swap the buffers
         glfwSwapBuffers( window );
     }
     
-    glDeleteVertexArrays( 1, &containerVAO );
-    glDeleteVertexArrays( 1, &lightVAO );
-    glDeleteBuffers( 1, &VBO );
-    
-    // Terminate GLFW, clearing any resources allocated by GLFW.
     glfwTerminate( );
-    return EXIT_SUCCESS;
+    return 0;
 }
 
 // Moves/alters the camera positions based on user input
 void DoMovement( )
 {
     // Camera controls
-    if( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] )
+    if ( keys[GLFW_KEY_W] || keys[GLFW_KEY_UP] )
     {
         camera.ProcessKeyboard( FORWARD, deltaTime );
     }
     
-    if( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] )
+    if ( keys[GLFW_KEY_S] || keys[GLFW_KEY_DOWN] )
     {
         camera.ProcessKeyboard( BACKWARD, deltaTime );
     }
     
-    if( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] )
+    if ( keys[GLFW_KEY_A] || keys[GLFW_KEY_LEFT] )
     {
         camera.ProcessKeyboard( LEFT, deltaTime );
     }
     
-    if( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] )
+    if ( keys[GLFW_KEY_D] || keys[GLFW_KEY_RIGHT] )
     {
         camera.ProcessKeyboard( RIGHT, deltaTime );
     }
@@ -297,38 +173,18 @@ void DoMovement( )
 // Is called whenever a key is pressed/released via GLFW
 void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mode )
 {
-    if( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
+    if ( GLFW_KEY_ESCAPE == key && GLFW_PRESS == action )
     {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
     
-    if (key == GLFW_KEY_C && action == GLFW_PRESS) {
-        
-        cameraMode++;
-        if(cameraMode > 2)
-            
-            cameraMode = 0;
-        
-        else if(cameraMode == 1){
-            
-            camera.SetPositionTop();
-            
-        }
-        else if (cameraMode == 2){
-            
-            camera.SetPositionSide();
-        }
-        
-        
-    }
-    
     if ( key >= 0 && key < 1024 )
     {
-        if( action == GLFW_PRESS )
+        if ( action == GLFW_PRESS )
         {
             keys[key] = true;
         }
-        else if( action == GLFW_RELEASE )
+        else if ( action == GLFW_RELEASE )
         {
             keys[key] = false;
         }
@@ -337,7 +193,7 @@ void KeyCallback( GLFWwindow *window, int key, int scancode, int action, int mod
 
 void MouseCallback( GLFWwindow *window, double xPos, double yPos )
 {
-    if( firstMouse )
+    if ( firstMouse )
     {
         lastX = xPos;
         lastY = yPos;
@@ -352,9 +208,3 @@ void MouseCallback( GLFWwindow *window, double xPos, double yPos )
     
     camera.ProcessMouseMovement( xOffset, yOffset );
 }
-
-/*void ScrollCallback( GLFWwindow *window, double xOffset, double yOffset )
-{
-    camera.ProcessMouseScroll( yOffset );
-}
-*/
